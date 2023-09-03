ef4c70f933d05bd043f8f91d5da532411c43fece
Jesse <jvelas028@ucr.edu>
2023-09-03 15:26:38 -0700

    ship class

---
+++ src/js/shipClass.js
@@ -0,0 +1,26 @@
+class Ship {
+    constructor(length) {
+        this.length = length
+        this.hits = 0
+        this.sunk = false
+    }
+
+    hit() {
+        if (this.hits === this.length)
+        {
+            return
+        }
+        
+        this.hits += 1
+        this.isSunk()
+    }
+
+    isSunk() {
+        if (this.hits === this.length)
+        {
+            this.sunk = true
+        }
+    }
+}
+
+export {Ship}
