db15d9ee43a43dff3765b668f1298c93d2e53831
Jesse <jvelas028@ucr.edu>
2023-09-05 17:11:03 -0700

    updates

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import { Ship } from "./js/shipClass";
 import { Gameboard } from "./js/gameboardClass";
 
-// let nS = new Ship(3)
-// nS.hit()
-// nS.hit()
-// nS.hit()
-// nS.hit()
-// console.log(nS)
+let nS = new Ship(3)
+nS.hit()
+nS.hit()
+nS.hit()
+nS.hit()
+console.log(nS)
 
-// let bg = new Gameboard(2, 2)
-// bg.placeShip(0, 0, 2)
-// console.log(bg)
+const gb = new Gameboard(2, 2)
+let ship = gb.placeShip(0, 0)
+gb.receiveAttack(0, 0)
+console.log(gb)
